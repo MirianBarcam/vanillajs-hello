61b29188d348b8f5f8af8f1c11ff399fbe715c38
MirianBarcam <mirianbarcam@gmail.com>
2022-06-20 13:57:53 +0200

    exercise completed

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-   console.log("Hello Rigo from the console!");
-};
+
+
+let who = ['the dog','my granma','his turtle','my bird'];
+let what = ['eat','pissed','crushed','broked'];
+let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+
+
+let excuse = excuseGenerator();
+
+function excuseGenerator(){
+  let numWhoRandom = Math.floor((Math.random() * 3) + 1);
+  let numWhatRandom = Math.floor((Math.random() * 3) + 1);
+  let numWhenRandom = Math.floor((Math.random() * 4) + 1);
+  return who[numWhoRandom]+' '+what[numWhatRandom]+' my homeworks '+when[numWhenRandom];  
+}
+document.getElementById("excuse").innerHTML = excuse;
